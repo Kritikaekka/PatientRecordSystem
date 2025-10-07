b910e5de98c21aba0e2523c8fb41125018786f28
Kritika Ekka <121709627+Kritikaekka@users.noreply.github.com>
2025-10-07 16:41:50 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,9 @@
-require('dotenv').config();
-
-
 const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT  // This line is important for non-3306 ports!
 });
 module.exports = connection;
